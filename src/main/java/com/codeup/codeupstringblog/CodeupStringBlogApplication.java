cb880efed384224cb5cadeee91ef5d94ef3844b6
Logan Rodriguez <Rodriguezlogan0428@gmail.com>
2023-06-29 09:36:54 -0500

    first commit

---
+++ src/main/java/com/codeup/codeupstringblog/CodeupStringBlogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.codeupstringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeupStringBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeupStringBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/codeupstringblog/CodeupStringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.codeupstringblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeupStringBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
